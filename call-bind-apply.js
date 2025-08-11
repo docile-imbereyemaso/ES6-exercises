da50c1d7f2ff76bf878ec13fb07c7f71c7754852
Docile123 <imbereyemasoo@gmail.com>
2025-08-11 21:53:15 +0200

    feat: applying bind to capitalize letter

--- call-bind-apply.js
+++ call-bind-apply.js
@@ -6,4 +6,21 @@ const triangle = {width:20, height:30};
 
 const area = calculateArea.bind(triangle);
 
-console.log(area())
+//With help of apply find maximum number in array using apply
+
+const numbers =[34, 12, 57, 28, 91];
+
+const maximum = Math.max.apply(null,numbers);
+
+// console.log(maximum)
+
+// with help of call method, capitalize the first letter on john;
+
+function capitalize(){
+    return this.charAt(0).toUpperCase()+this.slice(1);
+}
+
+const name ="john";
+
+const capitalizeLetter = capitalize.call(name);
+console.log(capitalizeLetter)
