0b7b9a36cee1ced04ed8d429a5d424b734f0a496
Docile123 <imbereyemasoo@gmail.com>
2025-08-12 12:50:59 +0200

    feat:calculateExpression and string-addition

--- calcuteExpressions.js
+++ calcuteExpressions.js
@@ -2,3 +2,5 @@ const calculateExpression =(str)=>eval(str);
 
 console.log(calculateExpression("5 + 4 * 3 / 2 - 1"));
 console.log(calculateExpression("( 2 + 3 ) * ( 4 - 1 )"));
+
+// without using the eval
---
+++ string-addition.js
@@ -0,0 +1,4 @@
+const stringAddition =(string1,string2)=>`${string1} ${string2}`;
+
+console.log(stringAddition("Hello","Rwandans"));
+console.log(stringAddition(4444,5555));
