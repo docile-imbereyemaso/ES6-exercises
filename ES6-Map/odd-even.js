ff17a4b7a1afa50ff3f6a0fd4df468a503b515e0
Docile123 <imbereyemasoo@gmail.com>
2025-08-14 15:26:45 +0200

    feat:solving the kata using Map

--- ES6-Map/odd-even.js
+++ ES6-Map/odd-even.js
@@ -1,12 +1,22 @@
 const numbers = Array.from({ length: 50 }, (_, i) => i + 1);
-console.log(numbers)
+// console.log(numbers)
 
 const number = Array.from({length:50},(_,i)=>i+1);
-console.log(number);
+// console.log(number);
+
 
 const numberMap = new Map();
 numberMap.set("even",[]);
 numberMap.set("odd",[]);
 
-number
+number.forEach((num)=>{
+    if(num % 2 ===0){
+        numberMap.get("even").push(num);
+    }
+    else{
+        numberMap.get("odd").push(num);
+    }
+});
+
+console.log(numberMap.get("even"));
 
