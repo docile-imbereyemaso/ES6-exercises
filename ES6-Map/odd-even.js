c2db7bc36dc97449dbfc83142edcc16d57f294a8
Docile123 <imbereyemasoo@gmail.com>
2025-08-14 15:21:43 +0200

    feat:using Map to find the odd and even numbers

---
+++ ES6-Map/odd-even.js
@@ -0,0 +1,12 @@
+const numbers = Array.from({ length: 50 }, (_, i) => i + 1);
+console.log(numbers)
+
+const number = Array.from({length:50},(_,i)=>i+1);
+console.log(number);
+
+const numberMap = new Map();
+numberMap.set("even",[]);
+numberMap.set("odd",[]);
+
+number
+
